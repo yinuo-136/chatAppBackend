f4fa29fcbc9012b5890b172194828af98e9d6818
Ian Hou <z5324093@ad.unsw.edu.au.com>
2021-09-21 02:05:37 +1000

    implement the test input feature of the function channel_create_v1 and pass the test(1/1)

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,7 @@
+from src.error import InputError
+from src.error import AccessError
+
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +23,9 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+    if len(name) < 1 or len(name) > 20:
+        raise(InputError)
+        
     return {
         'channel_id': 1,
     }
