1c5449a724d37aa38aa0a7e35409f62edc53e7b9
Ian Hou <z5324093@ad.unsw.edu.au.com>
2021-09-22 01:10:04 +1000

    add three other tests to test different features; add one assumption about function channels_create_v1; modified the data store structure

---
+++ assumptions.md
@@ -0,0 +1,3 @@
+channels_create_v1 assuptions:
+    1. eachtime the channel_id that created by the function should  be unique.
+    2. 
--- tests/channels_test.py
+++ tests/channels_test.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.channels import channels_create_v1
 from src.auth import auth_register_v1
-from src.error import InputError
+from src.error import AccessError, InputError
 from src.other import clear_v1
 
 # channels_create_v1 feature 1: length of name is less than 1 or more than 20 characters, 
@@ -15,15 +15,41 @@ def test_user_name():
 
     #check if the error raises if length of the name is less than 1
     with pytest.raises(InputError):
-        assert channels_create_v1(u_id, "", False)
+        channels_create_v1(u_id, "", False)
     
     #chcek if the error raises if length of the name is more than 20
     with pytest.raises(InputError):
-        assert channels_create_v1(u_id, "abcdefghijklmnopqrstuvwxyz", False) 
+        channels_create_v1(u_id, "abcdefghijklmnopqrstuvwxyz", False) 
 
+# channels_create_v1 feature 2: if the user didn't input the correct u_id（not exist), raise an 
+# AccessError.
+def test_uid_validity():
 
+    clear_v1()
 
+    u_id = 12
 
-# channels_create_v1 feature 2:
-# channels_create_v1 feature 3:
-# channels_create_v1 feature 4:
+    with pytest.raises(AccessError):
+        channels_create_v1(u_id, "correct_name", False)
+
+# channels_create_v1 feature 3: if both InputError(caused by name) and AccessError(caused by u_id)
+#  should've raised, raised AccessError
+def test_error_raised():
+    
+    clear_v1()
+
+    u_id = 12
+
+    with pytest.raises(AccessError):
+        channels_create_v1(u_id,"", False)
+
+# channels_create_v1 feature 4:  eachtime the channel_id that created by the function should
+#  be unique.
+def test_channel_id_unique():
+
+    clear_v1()
+
+    u_id = auth_register_v1("test@gmail.com", "password", "First", "Last")
+    c_id_1 = channels_create_v1(u_id, "correct_name", False)
+    c_id_2 = channels_create_v1(u_id, "correct_name_1", False)
+    assert c_id_1 != c_id_2
