e8e2f7f81cba75b38fe94a6991fae65394f27292
Ian Hou <z5324093@ad.unsw.edu.au.com>
2021-09-25 14:28:05 +1000

    modify the tests and add the implementation of user id check

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -7,11 +7,12 @@ from src.other import clear_v1
 
 # channels_create_v1 feature 1: length of name is less than 1 or more than 20 characters, 
 # if it fails the rule, raise an InputError.
-def test_user_name():
+def test_user_name_validity():
 
     clear_v1()
 
-    u_id = auth_register_v1("test@gmail.com", "password", "First", "Last")
+    u_dict = auth_register_v1("test@gmail.com", "password", "First", "Last")
+    u_id = u_dict['auth_user_id']
 
     #check if the error raises if length of the name is less than 1
     with pytest.raises(InputError):
@@ -21,12 +22,14 @@ def test_user_name():
     with pytest.raises(InputError):
         channels_create_v1(u_id, "abcdefghijklmnopqrstuvwxyz", False) 
 
+
 # channels_create_v1 feature 2: if the user didn't input the correct u_id（not exist), raise an 
 # AccessError.
 def test_uid_validity():
 
     clear_v1()
 
+    # a random number that doesn't exist.
     u_id = 12
 
     with pytest.raises(AccessError):
@@ -34,7 +37,7 @@ def test_uid_validity():
 
 # channels_create_v1 feature 3: if both InputError(caused by name) and AccessError(caused by u_id)
 #  should've raised, raised AccessError
-def test_error_raised():
+def test_which_error_raised():
     
     clear_v1()
 
@@ -42,6 +45,10 @@ def test_error_raised():
 
     with pytest.raises(AccessError):
         channels_create_v1(u_id,"", False)
+    
+    with pytest.raises(AccessError):
+        channels_create_v1(u_id, "akgoiq1214109r5172847129124-9091724asd", False)
+
 
 # channels_create_v1 feature 4:  eachtime the channel_id that created by the function should
 #  be unique.
@@ -49,7 +56,8 @@ def test_channel_id_unique():
 
     clear_v1()
 
-    u_id = auth_register_v1("test@gmail.com", "password", "First", "Last")
+    u_dict = auth_register_v1("test@gmail.com", "password", "First", "Last")
+    u_id = u_dict['auth_user_id']
     c_id_1 = channels_create_v1(u_id, "correct_name", False)
     c_id_2 = channels_create_v1(u_id, "correct_name_1", False)
     assert c_id_1 != c_id_2
