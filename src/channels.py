21163a556d157b2e3990cebb79c1340d8abb1240
Ian Hou <z5324093@ad.unsw.edu.au.com>
2021-09-25 15:02:13 +1000

    implement data storing section of the function channel_create

--- src/channels.py
+++ src/channels.py
@@ -35,7 +35,13 @@ def channels_create_v1(auth_user_id, name, is_public):
     # implement the name validity check
     if len(name) < 1 or len(name) > 20:
         raise InputError("Invalid name is entered, needs to be a name between 1 and 20 characters!")
+    
+    #store channel information into date_store
+    c_id = len(store['channels']) + 1
+    owner = [auth_user_id]
+    members = [auth_user_id]
+    store['channels'].update({c_id : (name, is_public, owner, members)})
         
     return {
-        'channel_id': 1,
+        'channel_id': c_id,
     }
