e2e8644af06c7c6fac1b02917f056569bb90a38b
Ian Hou <z5324093@ad.unsw.edu.au.com>
2021-09-25 14:28:05 +1000

    modify the tests and add the implementation of user id check

--- src/channels.py
+++ src/channels.py
@@ -1,5 +1,6 @@
 from src.error import InputError
 from src.error import AccessError
+from src.data_store import data_store
 
 
 def channels_list_v1(auth_user_id):
@@ -23,8 +24,17 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+
+    store = data_store.get()
+
+    u_dict = store['user_details']
+    # implement the user id validity check
+    if auth_user_id not in u_dict.keys():
+        raise AccessError("the user id you entered does not exist")
+
+    # implement the name validity check
     if len(name) < 1 or len(name) > 20:
-        raise(InputError)
+        raise InputError("Invalid name is entered, needs to be a name between 1 and 20 characters!")
         
     return {
         'channel_id': 1,
