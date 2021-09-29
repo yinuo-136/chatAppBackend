21b137a214503f024f7907be7c5478f580a1ae3f
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-09-29 20:52:03 +1000

    Added skeleton channel_test file with comments for future tests

---
+++ tests/channel_test.py
@@ -0,0 +1,27 @@
+from src.error import InputError
+from src.error import AccessError
+import src.data_store 
+import src.error
+from src.channel import channel_invite_v1
+from src.other import clear_v1
+
+'''
+`channel inv`
+Check auth user id is valid
+Check channel id is valid
+Check u_id is valid
+Check u_id is already member of channel details v1 (shouldnt already be member) [AccessError]
+Check whether auth user id is apart of channel
+
+`channel join`
+Check for valid user id
+Valid channel id
+Check for private channel access [AccessError]
+ '''
+
+
+#def test_basic_channel_invite_fail__user_id_invalid():
+# channel invite => register user 
+
+
+
