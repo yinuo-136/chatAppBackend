3e563c7ee62f80e1f513546f9bd26cfdf0464a79
Jayden Matthews <z5360350@ad.unsw.edu.au>
2021-09-29 19:49:37 +1000

    Adding 10/10 pylint auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,65 +1,122 @@
+'''
+This file contains functions for registering a user and logging in a user.
+'''
+
+import re
 from src.data_store import data_store
 from src.error import InputError
 from src.error import AccessError
-import re
+
+def check_user_details(password, name_first, name_last):
+    '''
+    Checks the validity/length of passwords and users first/last name
+
+    Arguments:
+        <password> (<string>)    - <password of registered user>
+        <name_first> (<string>)  - <first name of user>
+        <name_last> (<string>)   - <last name of user>
+
+    Exceptions:
+        InputError  - Occurs when password, name_first, or name_last are not of
+        adequate length.
+
+    Return Value:
+        Returns <None>
+    '''
+
+    #error checking for password
+    if len(password) < 6:
+        raise InputError()
+
+    #error checking for name
+    if len(name_first) > 50 or len(name_first) < 1:
+        raise InputError()
+
+    if len(name_last) > 50 or len(name_last) < 1:
+        raise InputError()
 
 def auth_login_v1(email, password):
+    '''
+    <Logs a user into Streams and appends them to logged_in_users member in
+    data_store, includes relevant error checking>
+
+    Arguments:
+        <email> (<string>)    - <email of the user>
+        <password> (<string>)    - <password of the user>
+
+    Exceptions:
+        InputError  - Occurs when email doesnt exist or password incorrect.
+
+    Return Value:
+        Returns <auth_user_id>
+    '''
+
     store = data_store.get()
     # if email not in store['registered_users'].keys() raise error
     if email not in store['registered_users'].keys():
         print("Email doesnt exist!")
         raise InputError()
-    
+
     # if email in store['registered_users'].keys(), but password not matching, raise error
-    elif password != store['registered_users'].get(email):
+    if password != store['registered_users'].get(email):
         print("Wrong Password")
         raise InputError()
-    
-    else:
-        store['logged_in_users'].append(store['user_ids'].get(email))
+
+    store['logged_in_users'].append(store['user_ids'].get(email))
+
     return {
         'auth_user_id': store['user_ids'].get(email),
     }
 
+
 def auth_register_v1(email, password, name_first, name_last):
+    '''
+    <Registers a new user with the Streams platform>
+
+    Arguments:
+        <email> (<string>)       - <email of the user>
+        <password> (<string>)    - <password of the user>
+        <name_first> (<string>)  - <first name of user>
+        <name_last> (<string>)   - <last name of user>
+        ...
+
+    Exceptions:
+        InputError  - Occurs when email is not valid format,
+        AccessError - Occurs when duplicate email is provided
+
+    Return Value:
+        Returns <auth_user_id>
+    '''
     store = data_store.get()
-    #implement error for email 
+    #implement error for email
     regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
-    
-    if re.fullmatch(regex, email) == None:
+
+    if re.fullmatch(regex, email) is None:
         raise InputError()
-    
+
     #implement error checking for duplicate
     if email in store['registered_users'].keys():
         raise AccessError()
-    
-    #error checking for password
-    
-    if len(password) < 6:
+
+    #MIGHT NEED TO REMOVE AS NAMES MAY CONTAIN NON-ALNUM
+    name_regex = r'^[A-Za-z0-9]*$'
+
+    if re.fullmatch(name_regex, name_first) is None:
         raise InputError()
-    
-    #error checking for name
-    if len(name_first) > 50 or len(name_first) < 1:
+
+    if re.fullmatch(name_regex, name_last) is None:
         raise InputError()
-        
-    if len(name_last) > 50 or len(name_last) < 1:
-        raise InputError()
-   
-    #error checking for non-alnum in name 
-    name_regex = r'^[A-Za-z0-9]*$'   
-    
-    if re.fullmatch(name_regex, name_first) == None:
-        raise InputError()
-    elif re.fullmatch(name_regex, name_last) == None:
-        raise InputError()
-    
+
+    #error checking for password and user name
+    check_user_details(password, name_first, name_last)
+
     #HANDLE implementation
     handle = name_first + name_last
     handle = handle.lower()
-    
+
     if len(handle) > 20:
         handle = handle[0:20]
-    
+
     handle_matches = 0
     for i in store['user_details'].keys():
         user = store['user_details'][i]
@@ -67,27 +124,27 @@ def auth_register_v1(email, password, name_first, name_last):
         print(temp_handle)
         if temp_handle == handle:
             handle_matches += 1
-    
-    if (handle_matches > 0):        
-        handle = handle + str(handle_matches - 1)    
-        
-    
+
+    if handle_matches > 0:
+        handle = handle + str(handle_matches - 1)
+
+
     # Storing Details in Datastore
-    new_id = len(store['registered_users']) + 1 
-    
+    new_id = len(store['registered_users']) + 1
+
     #implementation of global permissions (1 for Owner, 2 for Member)
     if len(store['registered_users'].keys()) == 0:
         store['global_permissions'].update({new_id: 1})
     else:
         store['global_permissions'].update({new_id: 2})
-    
-     
+
+
     store['user_details'].update({new_id : (email, password, name_first, name_last, handle)})
     store['registered_users'].update({email: password})
     store['user_ids'].update({email: new_id})
     print(store['user_details'])
     print(store['registered_users'])
-    
+
     return {
         'auth_user_id': new_id,
     }
