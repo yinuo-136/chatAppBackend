72ca388823fbe12d7f64a29ca9736624d1336f8f
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-09-30 23:04:25 +1000

    Part implementation of first test

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -2,13 +2,14 @@ import pytest
 
 from src.error import InputError
 from src.error import AccessError
-import src.data_store 
-import src.error
+
+from src.auth import auth_register_v1
+
+from src.channels import channels_create_v1
+
 from src.channel import channel_invite_v1
+from src.channel import channel_join_v1
 from src.other import clear_v1
-
-
-
 
 '''
 `channel inv`
@@ -23,6 +24,31 @@ Check for valid user id
 Valid channel id
 Check for private channel access [AccessError]
  '''
+
+'''
+
+Parameters:{ 
+    auth_user_id = the user ID of the user who is making the request, 
+    channel_id = unique id of channel which user is being invited to, 
+    u_id =  user ID of user who is being invited}
+
+'''
+
+def test_channel_inv__user_id_valid():
+
+    register_id_return = auth_register_v1("test@gmail.com", "password", "First", "Last")
+
+    auth_u_id = register_id_return['auth_user_id']
+
+    c_id = channels_create_v1(auth_u_id, "TestChannel", False)
+
+
+    unregistered_u_id = 999
+
+    with pytest.raises(InputError):
+        channel_invite_v1(auth_u_id, c_id, unregistered_u_id)
+
+
 
 
 #def test_basic_channel_invite_fail__user_id_invalid():
