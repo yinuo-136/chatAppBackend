af7c87463771a15431cd206079edb29da1d5a4d5
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-09-30 23:55:58 +1000

    Finished basic tests for channel_join according to interface spec

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -43,7 +43,7 @@ def clear__register_two_users():
 
 
 @pytest.fixture
-def clear__register_two_users__create_channel():
+def clear__register_two_users__create_channel_private():
 
     clear_v1()
 
@@ -53,7 +53,25 @@ def clear__register_two_users__create_channel():
     register_id_return_2 = auth_register_v1("test2@gmail.com", "password", "First", "Last")
     auth_u_id_2 = register_id_return_2['auth_user_id']
 
-    return_c_id = channels_create_v1(auth_u_id_1, "TestChannel", False)
+    return_c_id = channels_create_v1(auth_u_id_1, "TestChannel1", False)
+    c_id = return_c_id['channel_id']
+
+    return [auth_u_id_1, auth_u_id_2, c_id]
+
+
+
+@pytest.fixture
+def clear__register_two_users__create_channel_public():
+
+    clear_v1()
+
+    register_id_return_1 = auth_register_v1("test1@gmail.com", "password", "First", "Last")
+    auth_u_id_1 = register_id_return_1['auth_user_id']
+
+    register_id_return_2 = auth_register_v1("test2@gmail.com", "password", "First", "Last")
+    auth_u_id_2 = register_id_return_2['auth_user_id']
+
+    return_c_id = channels_create_v1(auth_u_id_1, "TestChannel2", True)
     c_id = return_c_id['channel_id']
 
     return [auth_u_id_1, auth_u_id_2, c_id]
@@ -99,9 +117,9 @@ def test_channel_inv__u_id_already_member(clear__register_two_users):
         channel_invite_v1(auth_u_id_1, c_id, auth_u_id_2)
 
 
-def test_channel_invite__channel_id_valid__auth_not_member(clear__register_two_users__create_channel):
+def test_channel_invite__channel_id_valid__auth_not_member(clear__register_two_users__create_channel_private):
 
-    u_id_1, u_id_2, c_id = clear__register_two_users__create_channel
+    u_id_1, u_id_2, c_id = clear__register_two_users__create_channel_private
 
 
     new_user = auth_register_v1("random@gmail.com", "password", "First", "Last")
@@ -109,6 +127,59 @@ def test_channel_invite__channel_id_valid__auth_not_member(clear__register_two_u
 
     with pytest.raises(AccessError):
         channel_invite_v1(new_u_id, c_id, u_id_2)
+
+
+
+############# CHANNEL JOIN TESTS
+
+# channel_id refers to a channel that is private and the authorised user is not already a channel member and is not a global owner
+def test_channel_join__auth_u_id_invalid__private(clear__register_two_users__create_channel_private):
+    
+    u_id_1, u_id_2, c_id = clear__register_two_users__create_channel_private
+
+    with pytest.raises(AccessError):
+        channel_join_v1(u_id_2, c_id)
+
+
+
+def test_channel_join__auth_u_id_valid__public(clear__register_two_users__create_channel_public):
+    
+    u_id_1, u_id_2, c_id = clear__register_two_users__create_channel_public
+
+    channel_join_v1(u_id_2, c_id)
+
+
+def test_channel_join__auth_u_id_valid__public(clear__register_two_users):
+    
+    u_id_1, u_id_2 = clear__register_two_users
+
+
+    return_channel_details = channels_create_v1(u_id_2, "TestChannel", False)
+    c_id = return_channel_details['channel_id']
+
+    # should work even though channel is private as user 1 is a global owner
+    channel_join_v1(u_id_1, c_id)
+
+
+def test_channel_join__channel_id_invalid(clear__register_two_users):
+
+    u_id_1, u_id_2 = clear__register_two_users
+
+    invalid_c_id = 999
+
+    with pytest.raises(InputError):
+        channel_join_v1(u_id_1, invalid_c_id)
+
+
+
+def test_channel_join__auth_user_already_member_invalid(clear__register_user__create_channel):
+
+    u_id, c_id = clear__register_user__create_channel
+
+    # user who created it is trying to join the same channel, error!
+    with pytest.raises(InputError):
+        channel_join_v1(u_id, c_id)
+
 
 '''
 `channel inv`
