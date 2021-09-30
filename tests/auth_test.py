4fb3f0f19f60d6549fc5803e858031c810ae8b8f
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-09-30 11:21:03 +1000

    Improved entire file, added comments and refactored most tests to be purely black box, with no data_store being accessed

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -5,58 +5,55 @@ from src.auth import auth_login_v1
 from src.error import InputError
 from src.error import AccessError
 from src.other import clear_v1
-from src.data_store import *
+
+# import pytest fixture
+from tests.clear_test import clear_and_register_single_user
 
 
-# uses method clear_v1 and registers a user with the details:
-#       email: test@gmail.com
-#       password: password
-#       first_name: First
-#       last_name: Last
-@pytest.fixture
-def clear_and_register_single_user():
+################# START OF BLACKBOX TESTS #################
 
-    clear_v1()
-
-    result = auth_register_v1("test@gmail.com", "password", "First", "Last")
-
-    return result
-
-
-
+# test that a single user can successfully register
 def test_basic_successful_register__single(clear_and_register_single_user):
 
-    assert clear_and_register_single_user == { 'auth_user_id' : 1 }
+    clear_and_register_single_user
 
 
+# test multiple users can register successfully
 def test_basic_successful_register__multiple():
 
     clear_v1()
 
-    assert auth_register_v1("email@gmail.com", "password", "Jayden", "Matthews") == { 'auth_user_id' : 1 }
-    assert auth_register_v1("email@yahoo.com", "password", "Nick", "Stathakis") == { 'auth_user_id' : 2 }
-    assert auth_register_v1("email@outlook.com", "password", "Sample", "Name") == { 'auth_user_id' : 3 }
+    # register 3 different users, should all work successfully (no exceptions raised)
+    auth_register_v1("test1@gmail.com", "password", "Person", "Lastname")
+    auth_register_v1("test2@gmail.com", "password", "Person", "Lastname")
+    auth_register_v1("test3@gmail.com", "password", "Person", "Lastname")
 
 
+# test that if a duplicate email is detected then it fails
 def test_basic_failed_register__duplicates(clear_and_register_single_user):
 
+    # registers a user with email = "test@gmail.com"
     clear_and_register_single_user
 
+    # registers another user with the SAME email, causing an AccessError
     with pytest.raises(AccessError):
         auth_register_v1("test@gmail.com", "password", "First", "Last")
 
 
+# tests the workflow of registering and login, making sure the data returned is consistent
 def test_full_register_and_login_successful(clear_and_register_single_user):
 
-    register_details = clear_and_register_single_user
-    auth_user_id_reg = register_details["auth_user_id"]
+    # register user1 and store the user id returned
+    u_id_register = clear_and_register_single_user
 
-    login_details = auth_login_v1("test@gmail.com", "password")
-    auth_user_id_log = login_details["auth_user_id"]
+    # login to user1 and store user id returned
+    u_id_login = auth_login_v1("test@gmail.com", "password")
 
-    assert auth_user_id_log == auth_user_id_reg
+    # make sure these two id's are the same
+    assert u_id_register == u_id_login
 
 
+# test that an incorrect email throws an InputError during LOGIN
 def test_failed_login__email_invalid(clear_and_register_single_user):
 
     clear_and_register_single_user
@@ -65,6 +62,7 @@ def test_failed_login__email_invalid(clear_and_register_single_user):
         auth_login_v1("not_correct@yahoo.com", "password")
 
 
+# test that an incorrect password throws an InputError during LOGIN
 def test_failed_login__password_invalid(clear_and_register_single_user):
 
     clear_and_register_single_user
@@ -73,6 +71,7 @@ def test_failed_login__password_invalid(clear_and_register_single_user):
         auth_login_v1("test@gmail.com", "not_correct")
 
 
+# test that an invalid email fails during REGISTRATION
 def test_failed_register__email_invalid():
 
     clear_v1()
@@ -81,22 +80,27 @@ def test_failed_register__email_invalid():
         auth_register_v1("invalid_email", "password", "Nick", "Stathakis")
 
 
+# test that a duplicate email during registration FAILS and throws an AccessError
 def test_failed_register__duplicate_email(clear_and_register_single_user):
 
+    # register a user with the email = "test@gmail.com"
     clear_and_register_single_user
 
     with pytest.raises(AccessError):
         auth_register_v1("test@gmail.com", "another_password", "Jayden", "Matthews")
 
 
+# test that an InputError is thrown when a password is invalid -- too small
 def test_failed_register__small_password():
 
     clear_v1()
 
+    # an invalid password of 4 characters (must be greater than 6)
     with pytest.raises(InputError):
         auth_register_v1("test@gmail.com", "four", "Nick", "Stathakis")
 
 
+# test that a user can successfully register with a 50 character First name (the limit)
 def test_successful_register__long_first():
 
     clear_v1()
@@ -104,6 +108,7 @@ def test_successful_register__long_first():
     auth_register_v1("test@gmail.com", "password", "a" * 50, "Stathakis")
 
 
+# test that an empty first name throws an InputError exception
 def test_failed_register__empty_first():
 
     clear_v1()
@@ -112,6 +117,7 @@ def test_failed_register__empty_first():
         auth_register_v1("test@gmail.com", "password", "", "Stathakis")
 
 
+# test that a first name GREATER than 50 characters throws an InputError
 def test_failed_register__first_more_than_fifty():
 
     clear_v1()
@@ -120,6 +126,7 @@ def test_failed_register__first_more_than_fifty():
         auth_register_v1("test@gmail.com", "password", "a" * 51, "Stathakis")
 
 
+# test that a first name with invalid characters, such as symbols, will throw an InputError
 def test_failed_register__first_invalid_characters():
 
     clear_v1()
@@ -128,6 +135,7 @@ def test_failed_register__first_invalid_characters():
         auth_register_v1("test@gmail.com", "password", "I$Am_Invalid+-", "Stathakis")
 
 
+# test that the maximum characters of last name (50) is valid
 def test_success_register__last_long():
 
     clear_v1()
@@ -135,6 +143,7 @@ def test_success_register__last_long():
     auth_register_v1("test@gmail.com", "password", "FirstName", "a" * 50)
 
 
+# test that if the last name is empty then a InputError is thrown
 def test_failed_register__last_empty():
 
     clear_v1()
@@ -143,6 +152,7 @@ def test_failed_register__last_empty():
         auth_register_v1("test@gmail.com", "password", "FirstName", "")
 
 
+# test that if the last name exceeds the maximum of 50 characters, an InputError is thrown
 def test_failed_register__last_more_than_fifty():
 
     clear_v1()
@@ -151,6 +161,7 @@ def test_failed_register__last_more_than_fifty():
         auth_register_v1("test@gmail.com", "password", "FirstName", "a" * 51)
 
 
+# test that is the last name has invalid characters, such as symbols, then an InputError is thrown
 def test_failed_register__last_invalid_characters():
 
     clear_v1()
@@ -159,73 +170,4 @@ def test_failed_register__last_invalid_characters():
         auth_register_v1("test@gmail.com", "password", "FirstName", "I$Am_Invalid+-")
 
 
-def test_basic_valid_handle(clear_and_register_single_user):
-
-    clear_and_register_single_user
-    
-    store = data_store.get()
-    
-    user = store['user_details'].get(1)
-    assert user[4] == "firstlast"
-    
-    
-
-def test_basic_handle__duplicate():
-
-    clear_v1()
-
-    auth_register_v1("email1@gmail.com", "password1" , "Jayden" , "Matthews")
-    auth_register_v1("email2@gmail.com", "password2" , "Jayden" , "Matthews")
-    auth_register_v1("email3@gmail.com", "password3" , "Jayden" , "Matthews")
-    auth_register_v1("email4@gmail.com", "password4" , "Jayden" , "Matthews")
-    
-    store = data_store.get()
-    
-    user = store['user_details'].get(1)
-    assert user[4] == "jaydenmatthews"  
-
-    user = store['user_details'].get(2)
-    assert user[4] == "jaydenmatthews0"
-
-    user = store['user_details'].get(3)
-    assert user[4] == "jaydenmatthews1"
-
-    user = store['user_details'].get(4)
-    assert user[4] == "jaydenmatthews2"
-    
-    
-
-
-def test_valid_handle__long_name():
-
-    clear_v1()
-
-    auth_register_v1("email@gmail.com", "password" , "a" * 19 , "b" * 10)
-    
-    store = data_store.get()
-    
-    user = store['user_details'].get(1)
-    assert user[4] == "a" * 19 + "b"
-    
-    
-
-   
-def test_basic_global_permissions():
-
-    clear_v1()
-
-    auth_register_v1("email1@gmail.com", "password1" , "Jayden" , "Matthews")
-    auth_register_v1("email2@gmail.com", "password2" , "Nick" , "Stath")
-    auth_register_v1("email3@gmail.com", "password3" , "Other" , "Guy")
-    
-    store = data_store.get()
-    
-    user_id1 = store['user_ids'].get("email1@gmail.com")
-    user_id2 = store['user_ids'].get("email2@gmail.com")    
-    user_id3 = store['user_ids'].get("email3@gmail.com")    
-    
-    assert store['global_permissions'].get(user_id1) == 1
-    assert store['global_permissions'].get(user_id2) == 2
-    assert store['global_permissions'].get(user_id3) == 2
-    
-    
+############### End of Blackbox testing -- see whitebox/auth_white_test.py for whitebox tests
