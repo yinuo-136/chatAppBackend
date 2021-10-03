1c6d60854e6d901fabbd8376a47f8bb6c48a73c1
Jayden Matthews <jayden.matthews@student.unsw.edu.au>
2021-10-03 12:54:49 +1100

    added error messages and fixed redundant elif channel.py

--- src/channel.py
+++ src/channel.py
@@ -27,24 +27,24 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
 
     #check auth_user_id is valid
     if auth_user_id not in store['user_details'].keys():
-        raise AccessError()
+        raise AccessError("Autherised user ID is not valid")
 
     #check channel_id is valid
     if channel_id not in store['channels'].keys():
-        raise InputError()
+        raise InputError("channel ID is not valid")
 
     #check u_id is valid
     if u_id not in store['user_details'].keys():
-        raise InputError()
+        raise InputError("The u_id is not valid")
 
     #check u_id is already member of channel_details_v1
     channel = store['channels'].get(channel_id)
     if (u_id in channel[2]) or (u_id in channel[3]):
-        raise InputError()
+        raise InputError("The u_id is is already a member of the channel")
 
     #check whether auth_user_id is a member of the channel
     if auth_user_id not in channel[3]:
-        raise AccessError()
+        raise AccessError("Auth_User is not a member of the channel")
 
     
     members = channel[3]
@@ -216,23 +216,21 @@ def channel_join_v1(auth_user_id, channel_id):
 
     #check for valid auth_user_id
     if auth_user_id not in store['user_details'].keys():
-        raise AccessError()
+        raise AccessError("auth_user_id is not valid")
 
 
     #error checking for invalid channel_id
     if channel_id not in store['channels'].keys():
-        raise InputError()
+        raise InputError("channel_id is not valid")
 
 
     #error checking for if user is already member
-    if auth_user_id in channel[2]:
-        raise InputError()
-    elif auth_user_id in channel[3]:
-        raise InputError()
+    if auth_user_id in channel[3]:
+        raise InputError("Auth_user is already a member")
 
     #error checking for private channel access
     if channel[1] == False and store['global_permissions'].get(auth_user_id) != 1:
-        raise AccessError()
+        raise AccessError("This channel is private and you do not have global permissions")
 
     members = channel[3]
     members.append(auth_user_id)
