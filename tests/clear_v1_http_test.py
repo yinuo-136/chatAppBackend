b2206d1acd8ce15e8ad4d1851dcb0d94954c81ef
Jayden Matthews <jayden.matthews@student.unsw.edu.au>
2021-10-19 10:57:03 +1100

    Added use of wrapper clear_v1_http_test.py

--- tests/clear_v1_http_test.py
+++ tests/clear_v1_http_test.py
@@ -1,12 +1,13 @@
 import requests
 import json
 from src import config
+from wrapper.clear_wrapper import clear_http
 
 BASE_URL = config.url
 
 def test_clear__correct_return():
 
-    r = requests.delete(BASE_URL + "clear/v1")
+    r = clear_http()
 
     status_code = r.status_code
     response_data = json.loads(r.text)
