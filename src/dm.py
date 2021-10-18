a388fb2ba83683f68079629f473431e687177529
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-10-18 17:48:33 +1100

    Added stubbed code for dm_create_v1 with a docstring from the interface spec.

---
+++ src/dm.py
@@ -0,0 +1,18 @@
+from src.error import InputError
+from src.error import AccessError
+from src.data_store import data_store
+
+# TODO(nick): this function. It is currently a stub.
+def dm_create_v1():
+
+    '''
+    u_ids contains the user(s) that this DM is directed to, and will not include the creator. 
+    
+    The creator is the owner of the DM. name should be automatically generated based on the users that are in this DM. 
+    
+    The name should be an alphabetically-sorted, comma-and-space-separated list of user handles, e.g. 'ahandle1, bhandle2, chandle3'.
+    
+    '''
+
+    # dummy code for `dm_id` return
+    return { 42 }
