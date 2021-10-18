fd8b1560222c6a7009135ec4fc09d14435b4db0b
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-10-18 20:13:48 +1100

    Added more tests which target features mentioned in spec interface

--- tests/dm_test.py
+++ tests/dm_test.py
@@ -5,10 +5,13 @@ from src import config
 from src.dm import dm_create_v1
 
 BASE_URL = config.url
+
+
+# Status code constants
 ACCESS_ERROR_CODE = 403
 INPUT_ERROR_CODE = 400
+SUCCESS = 200
 # AccessError       code = 403
-
 # InputError        code = 400
 
 '''
@@ -23,12 +26,13 @@ INPUT_ERROR_CODE = 400
 #   InputError when: any u_id in u_ids does not refer to a valid user
 def test_dm_create__fail__user_not_valid():
 
-    # payload = {'name' : 'Nick'}
-    # payload = json.dumps(payload)
-    # r = requests.post(BASE_URL + "names/add", data=payload)
-    # assert r.status_code == 200
+    #TODO: clear, register a user, then call the function with user token and invalid_id
 
-    payload = {'token' : 'sometoken', "u_ids" : [1, 2, 3, 4]} 
+    my_user_token = "xxx"
+    invalid_user_id = 99
+
+
+    payload = {'token' : my_user_token, 'u_ids' : [invalid_user_id]} 
     payload = json.dumps(payload)
 
     r = requests.post(BASE_URL + "dm/create/v1", data=payload)
@@ -36,3 +40,66 @@ def test_dm_create__fail__user_not_valid():
     status_code = r.status_code
 
     assert status_code == INPUT_ERROR_CODE #input-error
+
+
+#successful dm creation
+def test_dm_create__success_basic():
+
+    # TODO: clear, register two users, then post
+
+    my_user_token = "xxx"
+    valid_other_id = 1
+
+
+    payload = {'token' : my_user_token, 'u_ids' : [valid_other_id]} 
+    payload = json.dumps(payload)
+
+    r = requests.post(BASE_URL + "dm/create/v1", data=payload)
+
+    status_code = r.status_code
+    response_dict = json.loads(r.text)
+
+    assert status_code == SUCCESS # aka 200 OK
+    assert response_dict == { 'dm_id' : 1 } # should start at 1
+
+
+def test_dm_create__success__double_dm():
+
+    # TODO: clear, register two users
+
+
+    ############################ FIRST DM
+
+    my_user_token = "xxx"
+    valid_other_id = 1
+
+    payload = {'token' : my_user_token, 'u_ids' : [valid_other_id]} 
+    payload = json.dumps(payload)
+
+    r = requests.post(BASE_URL + "dm/create/v1", data=payload)
+
+    status_code = r.status_code
+    response_dict = json.loads(r.text)
+
+    assert status_code == SUCCESS # aka 200 OK
+    assert response_dict == { 'dm_id' : 1 } # should start at 1
+
+
+    ################################# SECOND DM
+
+    my_user_token = "yyy"
+    valid_other_id = 2
+
+    payload = {'token' : my_user_token, 'u_ids' : [valid_other_id]} 
+    payload = json.dumps(payload)
+
+    r = requests.post(BASE_URL + "dm/create/v1", data=payload)
+
+    status_code = r.status_code
+    response_dict = json.loads(r.text)
+
+    assert status_code == SUCCESS # aka 200 OK
+    assert response_dict == { 'dm_id' : 2 } # NEXT SHOULD BE 2 ID
+
+
+#Note: cannot test that name of DM will be alphabetically sorted as that would break blackbox
