9930cf4d9e42c5a4c8003bb1f40486628973db39
Ian Hou <z5324093@ad.unsw.edu.au.com>
2021-10-20 00:49:50 +1100

    fix the pipeline to pass the pylint

--- src/token.py
+++ src/token.py
@@ -6,7 +6,7 @@ from src.config import SECRET
 def token_checker(token):
     payload = jwt.decode(token, SECRET, algorithms=["HS256"])
     user_id = payload.get('user_id')
-    session_id = payload.get('session_id')
+    #session_id = payload.get('session_id')
     
     store = data_store.get()
     
@@ -14,11 +14,11 @@ def token_checker(token):
     if user_id not in store['user_details'].keys():
         raise AccessError("Invalid Token Passed: user_id does not exist")
     # User_id not logged in
-    #if user_id not in store['logged_in_users']:
-        #raise AccessError("Invalid Token Passed: user_id specified is not logged in")
+    if user_id not in store['logged_in_users']:
+        raise AccessError("Invalid Token Passed: user_id specified is not logged in")
     # Session_id doesnt exist
-    if session_id not in store['session_ids']:
-        raise AccessError("Invalid Token Passed: session_id does not exist")
+    #if session_id not in store['session_ids']:
+        #raise AccessError("Invalid Token Passed: session_id does not exist")
     
     
         
