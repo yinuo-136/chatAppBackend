09d743712248e3dc6dff92b55c2ea035a1561a59
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-10-21 00:14:16 +1100

    Modified data store to include 'messages' in preparation for final function dm_messages_v1

--- src/other.py
+++ src/other.py
@@ -11,6 +11,7 @@ def clear_v1():
     store['global_permissions'] = {}
     store['session_ids'] = []
     store['dms'] = {}
-
+    store['messages'] = []
+    
     data_store.set(store)
     return {}
