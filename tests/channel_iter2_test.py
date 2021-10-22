9a7d97ceff5feed9f6d5a4e74821662eacc5dd79
Jayden Matthews <jayden.matthews@student.unsw.edu.au>
2021-10-23 00:32:41 +1100

    removed unnecessary stuff channel_iter2_test.py

--- tests/channel_iter2_test.py
+++ tests/channel_iter2_test.py
@@ -6,30 +6,6 @@ from wrapper.auth_wrappers import auth_register, auth_login, auth_logout
 from wrapper.channel_wrappers import channel_details, channel_join, channel_invite
 from wrapper.channels_wrappers import user_create_channel 
 from wrapper.clear_wrapper import clear_http
-
-
-
-@pytest.fixture
-def channel_setup_register_user():
-    clear_http()
-    #r = auth_register("test1@gmail.com", "password123", "John", "Smith")
-    
-
-@pytest.fixture
-def create_channel_public():
-    clear_http()
-    #r = user_create_channel("token", "PublicChannel1", TRUE)
-    
-@pytest.fixture
-def create_channel_private():
-    clear_http()
-    #user_create_channel("token", "PublicChannel1", FALSE)
-	
-
-
-def test_basic_channel_details():
-    clear_http()
-    
     
 def test_channel_inv_channelid_invaild():
     clear_http()
