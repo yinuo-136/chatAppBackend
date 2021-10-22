d9b5442dfb6bf9dbded9169c25d09cdccc196bfd
Jayden Matthews <z5360350@ad.unsw.edu.au>
2021-10-23 01:30:12 +1100

    Added tests for channel_join, channel_invite and channel_list

--- tests/channel_iter2_test.py
+++ tests/channel_iter2_test.py
@@ -4,51 +4,208 @@ import json
 from src import config
 from wrapper.auth_wrappers import auth_register, auth_login, auth_logout
 from wrapper.channel_wrappers import channel_details, channel_join, channel_invite
-from wrapper.channels_wrappers import user_create_channel 
+from wrapper.channels_wrappers import user_create_channel, channels_list
 from wrapper.clear_wrapper import clear_http
-    
-def test_channel_inv_channelid_invaild():
-    clear_http()
-    r = auth_register("test1@gmail.com", "password123", "John", "Smith")
-    data = r.json()
-    user_token = data['token']
-    u_id = data['auth_user_id']
-    invalid_channel = 999
-    r1 = channel_invite(user_token, invalid_channel, u_id)
-    assert r1.status_code == 400
 
-def test_channel_join_channelid_invaild():
-    clear_http()
-    r = auth_register("test1@gmail.com", "password123", "John", "Smith")
-    data = r.json()
-    user_token = data['token']
-    invalid_channel = 999
-    r1 = channel_join(user_token, invalid_channel)
-    assert r1.status_code == 400
-   
-def test_channel_inv_user_id_invalid():
-    clear_http()
-    r = auth_register("test1@gmail.com", "password123", "John", "Smith")
-    data = r.json()
-    user_token = data['token']
-    invalid_uid = 999
-    channel = data['channel_id']
-    r1 = channel_invite(user_token, channel, invalid_uid)
-    assert r1.status_code == 404
 
-def test_channel_inv_uid_already_member():
-    clear_http()
-    r = auth_register("test1@gmail.com", "password123", "John", "Smith")
-    data = r.json()
-    user_token = data['token']
-    channel = data['channel_id']
-    u_id = data['auth_user_id']
-    c_id = user_create_channel(user_token, channel, False)
-    r1 = channel_invite(user_token, c_id, u_id)
-    assert r1.status_code == 404
-    #user_create_channel("token", "PublicChannel1", FALSE)
-    
+ACCESS_ERROR = 403
+INPUT_ERROR = 400
 
+def test_basic_join():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", True)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    token1 = r2.json()['token']
+    
+    r3 = channel_join(token1, c_id)
+    
+    assert r3.json() == {}
+    
+def test_basic_invite():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", False)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    u_id = r2.json()['auth_user_id']
+    
+    r3 = channel_invite(token, c_id, u_id)
+    
+    assert r3.json() == {}
+
+def test_invalid_channel_id_join():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", True)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    token1 = r2.json()['token']
+    
+    r3 = channel_join(token1, 999)
+    
+    assert r3.status_code == INPUT_ERROR
+    
+def test_user_alr_member_join():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", True)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    token1 = r2.json()['token']
+    
+    r3 = channel_join(token1, c_id)
+    
+    assert r3.json() == {}
+    
+    # Should fail as user already joined
+    r4 = channel_join(token1, c_id)
+    
+    assert r4.status_code == INPUT_ERROR
+    
+def test_private_ch_not_global_join():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", False)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    token1 = r2.json()['token']
+    
+    # Should fail as user isnt a global owner, and channel is private)
+    r3 = channel_join(token1, c_id)
+    
+    assert r3.status_code == ACCESS_ERROR
+
+def test_private_ch_is_global_join():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    token1 = r2.json()['token']
+    
+    c_id = user_create_channel(token1, "channelname", False)
+    
+    # Should work as user is a global owner, and channel is private)
+    r3 = channel_join(token, c_id)
+    
+    assert r3.json() == {}
+
+def test_invalid_channel_id_invite():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", False)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    u_id = r2.json()['auth_user_id']
+    
+    #Should fail due to invalid channel id
+    r3 = channel_invite(token, 999, u_id)
+    
+    assert r3.status_code == INPUT_ERROR
     
     
+def test_invalid_uid_invite():
+    clear_http()
+        
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
     
+    c_id = user_create_channel(token, "channelname", False)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+
+    #Should fail due to invalid user id
+    r3 = channel_invite(token, c_id, 123)
+    
+    assert r3.status_code == INPUT_ERROR
+    
+def test_uid_alr_member_invite():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", True)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    u_id = r2.json()['auth_user_id']
+    token1 = r2.json()['token']
+    
+    channel_join(token1, c_id)
+    
+    #Should fail as user has already joined 
+    r3 = channel_invite(token, c_id, u_id)
+    
+    assert r3.status_code == INPUT_ERROR
+    
+def test_auth_user_not_member_join():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id = user_create_channel(token, "channelname", False)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    token1 = r2.json()['token']
+    u_id = r2.json()['auth_user_id']
+    
+    r4 = auth_register("test3@gmail.com", "password123", "Nick", "Stath")
+    u_id1 = r4.json()['auth_user_id']
+    
+    # Should fail as Johnny Sins is not a member of channel 
+    r3 = channel_invite(token1, c_id, u_id1)
+    
+    assert r3.status_code == ACCESS_ERROR
+
+def test_basic_channel_list():
+    clear_http()
+    
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+    
+    c_id1 = user_create_channel(token, "channelname1", False)
+    c_id2 = user_create_channel(token, "channelname2", True)
+    c_id3 = user_create_channel(token, "channelname3", False)
+    
+    r2 = auth_register("test2@gmail.com", "password123", "Johnny", "Sins")
+    token1 = r2.json()['token']
+    
+    c_id4 = user_create_channel(token1, "channelname4", False)
+    
+    #Should list channelname1, channelname2, channelname3
+    r3 = channels_list(token)
+    
+    assert r3.json() == {'channels': [
+                                {'channel_id': c_id1,
+                                    'name': 'channelname1'
+                                },
+                                {'channel_id': c_id2,
+                                    'name': 'channelname2'
+                                },
+                                {'channel_id': c_id3,
+                                    'name': 'channelname3'
+                                }
+    ]}
+
--- wrapper/channel_wrappers.py
+++ wrapper/channel_wrappers.py
@@ -4,28 +4,18 @@ import requests
 import jwt
 from src import config
 
-def channel_details(user_id, channel_id):
-    pretoken = {
-        'user_id' : user_id,
-        'session_id' : 'assume_this_is_correct'
-    }
-
-    token = jwt.encode(pretoken, config.SECRET, algorithm = 'HS256')
+def channel_details(token, channel_id):
     payload = {'token' : token, 'channel_id' : channel_id}
 
     return requests.get(config.url + "channel/details/v2", params = payload)
     
 def channel_invite(token, channel_id, u_id):
-    payload = { 'token' : token,
-        'channel_id' : channel_id, 
-	'u_id' : u_id}
+    payload = { 'token' : token, 'channel_id' : channel_id, 'u_id' : u_id}
 	    
     return requests.post(config.url + "channel/invite/v2", json = payload)
 	
 def channel_join(token, channel_id):
-    payload = { 'token' : token,
-        'channel_id' : channel_id }
+    payload = { 'token' : token, 'channel_id' : channel_id }
 	    
     return requests.post(config.url + "channel/join/v2", json = payload)
 	
-
