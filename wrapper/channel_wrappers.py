4711b8a3d4365bbfe381a0194a5fd346fcacac68
Jayden Matthews <jayden.matthews@student.unsw.edu.au>
2021-10-22 17:31:40 +1100

    Update channel_wrappers.py

--- wrapper/channel_wrappers.py
+++ wrapper/channel_wrappers.py
@@ -15,14 +15,14 @@ def channel_details(user_id, channel_id):
 
     return requests.get(config.url + "channel/details/v2", params = payload)
     
-def channel_invite(token, channel_id, u_id)
+def channel_invite(token, channel_id, u_id):
     payload = { 'token' : token,
         'channel_id' : channel_id, 
 	'u_id' : u_id}
 	    
     return requests.post(config.url + "channel/invite/v2", json = payload)
 	
-def channel_join(token, channel_id)
+def channel_join(token, channel_id):
     payload = { 'token' : token,
         'channel_id' : channel_id }
 	    
