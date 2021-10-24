ffb80b3f0f3e481ea514d9ad3ff766041ac15e0f
Jayden Matthews <z5360350@ad.unsw.edu.au>
2021-10-24 19:31:11 +1100

    Pushed coverage to 99%

--- tests/admin_iter2_test.py
+++ tests/admin_iter2_test.py
@@ -6,8 +6,9 @@ from wrapper.clear_wrapper import clear_http
 from wrapper.admin_wrappers import admin_permission, admin_remove
 from wrapper.user_wrappers import user_profile, list_users
 from wrapper.channels_wrappers import user_create_channel
-from wrapper.channel_wrappers import channel_details
+from wrapper.channel_wrappers import channel_details, channel_join
 from wrapper.message_wrappers import send_message, show_messages
+from wrapper.dm_wrappers import dm_create_wrapper, dm_details_wrapper
 from src import config
 
 INPUT_ERROR = 400
@@ -46,9 +47,103 @@ def test_demote_permission():
     r4 = admin_permission(token1, 1, 2)
     assert r4.json() == {}
 
-#def test_channel_removal():
-#def test_dm_removal():   
-#def test_messages_change():
+def test_channel_member_remove():
+    clear_http()
+    r1 = auth_register("email@gmail.com", "password123", "Jayden", "Matthews")
+    token1 = r1.json()['token']
+    u_id = r1.json()['auth_user_id']
+    
+    r2 = auth_register("email1@gmail.com", "password123", "Nick", "Stath")
+    token2 = r2.json()['token']
+    
+    c_id = user_create_channel(token1, "channel1", True)
+    
+    channel_join(token2, c_id)
+    
+    send_message(token2, c_id, "This is a message from Nick")
+    
+    r3 = admin_remove(token1, 2)
+    assert r3.json() == {}
+    
+    r4 = show_messages(token1, c_id , 0)
+    assert r4[0] == "Removed user"
+    
+    r5 = channel_details(token1, c_id)
+    assert r5.json() == { 
+                    'name' : "channel1",
+                    'is_public' : True,
+                    'owner_members' : [user_profile(token1, u_id).json().get('user')],
+                    'all_members' : [user_profile(token1, u_id).json().get('user')]
+    }
+
+def test_channel_owner_remove():
+    clear_http()
+    r1 = auth_register("email@gmail.com", "password123", "Jayden", "Matthews")
+    token1 = r1.json()['token']
+    
+    r2 = auth_register("email1@gmail.com", "password123", "Nick", "Stath")
+    u_id2 = r2.json()['auth_user_id']
+    token2 = r2.json()['token']
+    
+    c_id = user_create_channel(token1, "channel1", True)
+    
+    channel_join(token2, c_id)
+    
+    r4 = admin_permission(token1, 2, 1)
+    assert r4.json() == {}
+    
+    r5 = admin_remove(token2, 1)
+    assert r5.json() == {}
+    
+    r6 = channel_details(token2, c_id)
+    assert r6.json() == { 
+                    'name' : "channel1",
+                    'is_public' : True,
+                    'owner_members' : [],
+                    'all_members' : [user_profile(token2, u_id2).json().get('user')]
+    }
+    
+def test_dm_member_remove(): 
+    clear_http()
+    r1 = auth_register("email@gmail.com", "password123", "Jayden", "Matthews")
+    token1 = r1.json()['token']
+    u_id = r1.json()['auth_user_id']
+    
+    r2 = auth_register("email1@gmail.com", "password123", "Nick", "Stath")
+    u_id2 = r2.json()['auth_user_id']
+    
+    r3 = dm_create_wrapper(token1, [u_id2])
+    dm_id = r3.json()['dm_id']
+    
+    r4 = admin_remove(token1, 2)
+    assert r4.json() == {}
+    
+    r5 = dm_details_wrapper(token1, dm_id)
+    assert r5.json()['members'] == [user_profile(token1, u_id).json().get('user')]
+    
+    
+def test_dm_owner_remove(): 
+    clear_http()
+    r1 = auth_register("email@gmail.com", "password123", "Jayden", "Matthews")
+    token1 = r1.json()['token']
+    u_id = r1.json()['auth_user_id']
+    
+    r2 = auth_register("email1@gmail.com", "password123", "Nick", "Stath")
+    token2 = r2.json()['token']
+    u_id2 = r2.json()['auth_user_id']
+    
+    r3 = dm_create_wrapper(token1, [u_id2])
+    dm_id = r3.json()['dm_id']
+    
+    r4 = admin_permission(token1, 2, 1)
+    assert r4.json() == {}
+    
+    r5 = admin_remove(token2, 1)
+    assert r5.json() == {}
+    
+    r6 = dm_details_wrapper(token2, dm_id)
+    assert r6.json()['members'] == [user_profile(token2, u_id2).json().get('user')]
+#def test_multiple_global_owner_remove():
 
 def test_user_profile_remove():
     clear_http()
