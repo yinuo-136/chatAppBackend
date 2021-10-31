537d24a20d4575a06bcb4a61e7ea1749c4272633
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-10-31 18:29:49 +1100

    Added skeleton comments and docstring for standup.py

---
+++ src/standup.py
@@ -0,0 +1,23 @@
+from src.error import InputError
+from src.error import AccessError
+from src.data_store import data_store
+from src.user import user_details
+from itertools import islice
+
+
+def standup_create_v1(u_id, c_id, length):
+
+    '''
+    Parameters:     { token, channel_id, length }
+    Return Type:    { time_finish }
+    
+    '''
+
+    #InputError when any of: channel_id does not refer to a valid channel
+    #InputError when any of: length is a negative integer
+    #InputError when any of: an active standup is currently running in the channel
+      
+    #AccessError when: channel_id is valid and the authorised user is not a member of the channel
+
+
+    return { 'time_finish' : 55 }
