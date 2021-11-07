cd93371432276aeb83094bc6eb17452f2d892bf0
Nicholas Stathakis <nicholas.stathakis1@gmail.com>
2021-11-07 22:43:39 +1100

    Fleshed out the tests for standup send further to handle edge cases

--- tests/standup_iter3_test.py
+++ tests/standup_iter3_test.py
@@ -236,6 +236,42 @@ def test_standup_active__fail__channel_id_invalid():
     assert status_code == INPUT_ERROR
 
 
+
+def test_standup_active__fail__user_not_member():
+
+    # Clear
+
+    clear_http()
+
+    # Register a user
+
+
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token1 = r1.json()['token']
+
+    r2 = auth_register("test2@gmail.com", "password123", "Nick", "Magalcky")
+    token2 = r2.json()['token']
+
+    # create a channel
+
+
+    c_id1 = user_create_channel(token1, "testchannel1", False)
+
+    # Start standup
+
+
+    standup_create_wrapper(token1, c_id1, 60) #User who is NOT MEMBER start standup
+    
+    # Other user check if standup active
+
+    standup_response = standup_is_active_wrapper(token2, c_id1)
+
+    status_code = standup_response.status_code
+
+    assert status_code == ACCESS_ERROR
+
+
+
 #################### BEGIN OF standup/send/v1
 
 
@@ -273,3 +309,130 @@ def test_standup_send__success__basic():
 
     assert status_code == SUCCESS
     assert response_body == {}
+
+
+
+def test_standup_send__fail__channel_id_invalid():
+
+     # Clear
+
+    clear_http()
+
+    # Register a user
+
+
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+
+    # run standup active method on an invalid channel
+
+    invalid_c_id = 9999
+
+    standup_response = standup_send_wrapper(token, invalid_c_id, "hey there") #create for 60 seconds
+
+    # check it returns 200 OK and a time_finish
+
+    status_code = standup_response.status_code
+
+    assert status_code == INPUT_ERROR
+
+
+
+def test_standup_send__fail__message_greater_than_thousand_chars():
+
+    #clear
+
+    clear_http()
+
+    # Register a user
+
+
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+
+    # create a channel
+
+
+    c_id1 = user_create_channel(token, "testchannel1", False)
+
+
+    # start a standup
+
+    standup_create_wrapper(token, c_id1, 60) #create for 60 seconds
+
+    # send message to standup
+
+    r = standup_send_wrapper(token, c_id1, "a" * 1001)
+
+    # check 200 OK
+
+    status_code = r.status_code
+   
+    assert status_code == INPUT_ERROR
+    
+
+
+def test_standup_send__fail__no_active_standup():
+
+
+    #clear
+
+    clear_http()
+
+    # Register a user
+
+
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token = r1.json()['token']
+
+    # create a channel
+
+
+    c_id1 = user_create_channel(token, "testchannel1", False)
+
+
+    # send message to NON-EXISTENT standup
+
+    r = standup_send_wrapper(token, c_id1, "this standup doesnt exist! hehehhe")
+
+    # check 200 OK
+
+    status_code = r.status_code
+   
+    assert status_code == INPUT_ERROR
+
+
+
+
+def test_standup_send__fail__user_not_member():
+
+    # Clear
+
+    clear_http()
+
+    # Register a user
+
+
+    r1 = auth_register("test1@gmail.com", "password123", "John", "Smith")
+    token1 = r1.json()['token']
+
+    r2 = auth_register("test2@gmail.com", "password123", "Nick", "Magalcky")
+    token2 = r2.json()['token']
+
+    # create a channel
+
+
+    c_id1 = user_create_channel(token1, "testchannel1", False)
+
+    # Start standup
+
+
+    standup_create_wrapper(token1, c_id1, 60) #User who is NOT MEMBER start standup
+    
+    # Other user check if standup active
+
+    standup_response = standup_send_wrapper(token2, c_id1, "im not a member. I should get an access error :(")
+
+    status_code = standup_response.status_code
+
+    assert status_code == ACCESS_ERROR
