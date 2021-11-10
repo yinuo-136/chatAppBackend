eaa10d84144ab60449de4f635f7f23e12959ec0d
z5205197 <z5205197@cse.unsw.edu.au>
2021-11-11 01:41:34 +1100

    fix pylint issue

--- src/data_store.py
+++ src/data_store.py
@@ -39,12 +39,7 @@ initial_object = {
     'standups' : {},
     'unique_codes' : [],
     'user_stats' : {},
-    'workspace_stats' : {
-        'channels_exist' : [{'num_channels_exist','time_stamp'}],
-        'dms_exist': [{'num_dms_exist','time_stamp'}],
-        'messages_exist': [{'num_messages_exist','time_stamp'}],
-        'utilization_rate'
-    },
+    'workspace_stats' : {},
 }
 ## YOU SHOULD MODIFY THIS OBJECT ABOVE
 
