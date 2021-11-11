fd333fdf12e13b67b6f90e945ea1431aeb1965e5
Benjamin <z3462131@ad.unsw.edu.au>
2021-11-11 12:42:29 +1100

    Updated logic for helper functions

--- src/user_stats.py
+++ src/user_stats.py
@@ -11,34 +11,34 @@ def current_time():
 def user_stats_channels_join(u_id):
     store = data_store.get()
     channels = store['user_stats'][u_id]['channels_joined']
-    num_channels = len(channels) + 1
+    num_channels = channels[-1]['num_channels_joined'] + 1
 
     channels.append({'num_channels_joined' : num_channels, 'time_stamp' : current_time()})
 
 def user_stats_channels_leave(u_id):
     store = data_store.get()
     channels = store['user_stats'][u_id]['channels_joined']
-    num_channels = len(channels) - 1
+    num_channels = channels[-1]['num_channels_joined'] - 1
 
     channels.append({'num_channels_joined' : num_channels, 'time_stamp' : current_time()})
 
 def user_stats_dms_join(u_id):
     store = data_store.get()
     dms = store['user_stats'][u_id]['dms_joined']
-    num_dms = len(dms) + 1
+    num_dms = dms[-1]['num_dms_joined'] + 1
 
     dms.append({'num_dms_joined' : num_dms, 'time_stamp' : current_time()})
 
 def user_stats_dms_leave(u_id):
     store = data_store.get()
     dms = store['user_stats'][u_id]['dms_joined']
-    num_dms = len(dms) - 1
+    num_dms = dms[-1]['num_dms_joined'] - 1
 
     dms.append({'num_dms_joined' : num_dms, 'time_stamp' : current_time()})
 
 def user_stats_messages(u_id):
     store = data_store.get()
     messages = store['user_stats'][u_id]['messages_sent']
-    num_messages = len(messages) + 1
+    num_messages = messages[-1]['num_messages_sent'] + 1
 
     messages.append({'num_messages_sent' : num_messages, 'time_stamp' : current_time()})
